c37de6fb0601b36b8a756eabea1b2620da214ca1
nowszy94 <nowszy94@gmail.com>
2017-08-29 21:04:38 +0200

    some scenarios with implementation

---
+++ src/test/java/com/sda/cucumber/stringcalcservice/StringCalcServiceRunner.java
@@ -0,0 +1,8 @@
+package com.sda.cucumber.stringcalcservice;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class StringCalcServiceRunner {
+}
--- src/test/java/com/sda/cucumber/writeservice/WriteServiceRunner.java
+++ src/test/java/com/sda/cucumber/writeservice/WriteServiceRunner.java
@@ -1,4 +1,4 @@
-package com.sda.cucumber;
+package com.sda.cucumber.writeservice;
 
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
